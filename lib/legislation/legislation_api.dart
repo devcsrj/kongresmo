3095dbfc1f01d3e205fd303f949b7c627aa5af57
Reijhanniel Jearl Campos <devcsrj@apache.org>
2018-12-01 15:20:01 +0800

    :bug: Fix continues streaming of bills

--- lib/legislation/legislation_api.dart
+++ lib/legislation/legislation_api.dart
@@ -1,3 +1,4 @@
+import 'package:html/dom.dart';
 import 'package:html/parser.dart' show parse;
 import 'package:http/http.dart' as http;
 import 'package:kongresmo_project/legislation/legislation.dart';
@@ -18,20 +19,36 @@ class SenateLegislationApi implements LegislationApi {
 
   @override
   Stream<Legislation> fetchAll(int congress) async* {
-    var uri = baseUri
-        .resolve("/lis/leg_sys.aspx?congress=$congress&type=bills&page=1");
-    var htmlBody = await httpClient.get(uri).then((response) => response.body);
-    var document = parse(htmlBody);
-    var entries = document.querySelectorAll("#form1 > div.alight > p");
-    for (var p in entries) {
-      var span = p.querySelector("a > span");
+    Document document = await _fetchDocumentByParam(congress, 1);
+    while (document != null) {
+      var entries = document.querySelectorAll("#form1 > div.alight > p");
+      for (var p in entries) {
+        var span = p.querySelector("a > span");
 
-      // SBN-2112: Exempting Medical Insurance Premiums From Income Tax and Fringe Benefit Tax
-      if (!span.text.contains(':')) continue;
-      var colon = span.text.indexOf(':');
-      var number = span.text.substring(0, colon);
-      var title = span.text.substring(colon + 1).trimLeft();
-      yield new Legislation(number, title);
+        // SBN-2112: Exempting Medical Insurance Premiums From Income Tax and Fringe Benefit Tax
+        if (!span.text.contains(':')) continue;
+        var colon = span.text.indexOf(':');
+        var number = span.text.substring(0, colon);
+        var title = span.text.substring(colon + 1).trimLeft();
+        yield new Legislation(number, title);
+      }
+      var element =
+          document.querySelector("#pnl_NavTop > div > div > a:last-child");
+      if (element == null || element.text.trim() != "Next") break;
+      var nextUri = Uri.parse(element.attributes["href"]);
+      document = await _fetchDocumentByUri(nextUri);
     }
+  }
+
+  Future<Document> _fetchDocumentByUri(Uri relativeUri) async {
+    var uri = baseUri.resolveUri(relativeUri);
+    return await httpClient.get(uri).then((response) {
+      return response.statusCode == 200 ? parse(response.body) : null;
+    });
+  }
+
+  Future<Document> _fetchDocumentByParam(int congress, int page) async {
+    return _fetchDocumentByUri(Uri.parse(
+        "/lis/leg_sys.aspx?congress=$congress&type=bills&page=$page"));
   }
 }
--- test/legislation/legislation_api_test.dart
+++ test/legislation/legislation_api_test.dart
@@ -12,12 +12,25 @@ void main() {
       server = new MockWebServer();
       await server.start();
     });
-    
+
     test('.fetchAll(17)', () {
       server.enqueueResponse(new MockResponse()
         ..httpCode = 200
         ..body = new File(
-                "test/resources/senate.gov.ph/17th-congress-bills-page1.htm")
+                "test/resources/senate.gov.ph/17th-congress-bills-page263.htm")
+            .readAsStringSync());
+      server.enqueueResponse(new MockResponse()
+        ..httpCode = 200
+        ..body = new File(
+                "test/resources/senate.gov.ph/17th-congress-bills-page264.htm")
+            .readAsStringSync());
+
+      // when requesting with a page that exceeds the maximum pages, the
+      // server returns the last page again, instead of a 404
+      server.enqueueResponse(new MockResponse()
+        ..httpCode = 200
+        ..body = new File(
+                "test/resources/senate.gov.ph/17th-congress-bills-page264.htm")
             .readAsStringSync());
 
       var api = new SenateLegislationApi(server.url);
@@ -26,15 +39,29 @@ void main() {
       expect(
           legislation,
           emitsInOrder([
-            new Legislation("SBN-2112",
-                "Exempting Medical Insurance Premiums From Income Tax and Fringe Benefit Tax"),
-            new Legislation("SBN-2111", "Public Solicitation Act"),
-            new Legislation("SBN-2110", "Long Term Care Fire Safety Act"),
-            new Legislation("SBN-2109", "Philippine Online Infringement Act"),
-            new Legislation("SBN-2108", "Poverty Reducation Through Social Entrepreneurship (Present) Act"),
-            new Legislation("SBN-2107", "Reducing the Value-Added Tax Rate to 10% Effective January 1, 2019"),
-            new Legislation("SBN-2106", "Cisfa Amendments of 2018"),
-            new Legislation("SBN-2105", "Regulation and Organization of Islamic Banks"),
+            new Legislation("SBN-16", "The New Central Bank Act"),
+            new Legislation("SBN-15", "Philippine Identification System Act"),
+            new Legislation("SBN-14",
+                "Amending the Revised Penal Code (Adjusting the Amount Involved on Which a Penalty Is Based)"),
+            new Legislation(
+                "SBN-13", "Salary Standardization Law of 2016 (Ssl 2016)"),
+            new Legislation(
+                "SBN-12", "Philippine Conditional Cash Transfer (CCT) Act"),
+            new Legislation("SBN-11", "Transportation Crisis Act of 2016"),
+            new Legislation("SBN-10", "National Internal Revenue Code of 1997"),
+            new Legislation("SBN-9", "Philippine Mental Health Act of 2016"),
+            // next page
+            new Legislation("SBN-8",
+                "Drug Rehabilitation Treatment for Philhealth Beneficiaries"),
+            new Legislation("SBN-7", "Sim Card Registration Act of 2016"),
+            new Legislation("SBN-6",
+                "R.A.No. 53, to Include Print, Broadcast and Electronic Mass Media in the Exemption"),
+            new Legislation("SBN-5", "Dangerous Drug Court (DDC)"),
+            new Legislation("SBN-4", "Death Penalty Act of 2016"),
+            new Legislation(
+                "SBN-3", "Presidential Anti-Drug Authority (Prada) Act"),
+            new Legislation("SBN-2", "14th Month Pay Law"),
+            new Legislation("SBN-1", "Anti-Drug Penal Institution"),
             emitsDone,
           ]));
     });
