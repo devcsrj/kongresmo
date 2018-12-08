696eec8d1b54c1c34fac33cccec0f8ece918c712
Reijhanniel Jearl Campos <devcsrj@apache.org>
2018-12-08 21:31:31 +0800

    :sparkles: Add util for converting numbers to words

---
+++ lib/legislation/utils.dart
@@ -0,0 +1,64 @@
+import 'dart:math';
+
+class Utils {
+  Utils() {
+    throw new AssertionError("Intentionally unimplemented");
+  }
+
+  /// Converts the provided {@code number} into its word equivalent.
+  /// e.g.: 17 becomes 'seventeenth'
+  static String ordinalOf(int number) {
+    // log10(number)
+    int places = (log(number) ~/ log(10));
+    if (places + 1 >= 3) {
+      throw new AssertionError(
+          "Oh wow, we're now on to $number Congress? Congrats PH!");
+    }
+
+    const singleDigitOrdinals = [
+      'first',
+      'second',
+      'third',
+      'fourth',
+      'fifth',
+      'sixth',
+      'seventh',
+      'eighth',
+      'ninth',
+      'tenth'
+    ];
+    if (number <= 10) return singleDigitOrdinals[number - 1];
+    const oneToTwentyOrdinals = [
+      'eleventh',
+      'twelfth',
+      'thirteenth',
+      'fourteenth',
+      'fifteenth',
+      'sixteenth',
+      'seventeenth',
+      'eighteenth',
+      'nineteenth',
+      'twentieth'
+    ];
+    if (number <= 20) return oneToTwentyOrdinals[number - 11];
+
+    const byTenOrdinals = [
+      '',
+      'twenty',
+      'thirty',
+      'forty',
+      'fifty',
+      'sixty',
+      'seventy',
+      'eighty',
+      'ninety'
+    ];
+    int tens = number ~/ 10;
+    String firstWord = byTenOrdinals[tens - 1];
+    int ones = number - (tens * 10);
+    if (ones == 0) return firstWord;
+
+    String secondWord = singleDigitOrdinals[ones - 1];
+    return firstWord + secondWord;
+  }
+}
---
+++ test/legislation/utils_test.dart
@@ -0,0 +1,40 @@
+import 'package:kongresmo_project/legislation/utils.dart';
+import 'package:test_api/test_api.dart';
+
+void main() {
+  test('ordinalOf(..)', () {
+    expect(Utils.ordinalOf(1), "first");
+    expect(Utils.ordinalOf(2), "second");
+    expect(Utils.ordinalOf(3), "third");
+    expect(Utils.ordinalOf(4), "fourth");
+    expect(Utils.ordinalOf(5), "fifth");
+    expect(Utils.ordinalOf(6), "sixth");
+    expect(Utils.ordinalOf(7), "seventh");
+    expect(Utils.ordinalOf(8), "eighth");
+    expect(Utils.ordinalOf(9), "ninth");
+    expect(Utils.ordinalOf(10), "tenth");
+    expect(Utils.ordinalOf(11), "eleventh");
+    expect(Utils.ordinalOf(12), "twelfth");
+    expect(Utils.ordinalOf(13), "thirteenth");
+    expect(Utils.ordinalOf(14), "fourteenth");
+    expect(Utils.ordinalOf(15), "fifteenth");
+    expect(Utils.ordinalOf(16), "sixteenth");
+    expect(Utils.ordinalOf(17), "seventeenth");
+    expect(Utils.ordinalOf(18), "eighteenth");
+    expect(Utils.ordinalOf(19), "nineteenth");
+    expect(Utils.ordinalOf(20), "twentieth");
+    expect(Utils.ordinalOf(21), "twentyfirst");
+
+    expect(Utils.ordinalOf(50), "fifty");
+    expect(Utils.ordinalOf(54), "fiftyfourth");
+  });
+
+  test('ordinalOf(100++) is so much wow', () {
+    try {
+      Utils.ordinalOf(100);
+      fail("Oops");
+    } on AssertionError catch (ex) {
+      expect(ex.message, "Oh wow, we're now on to 100 Congress? Congrats PH!");
+    }
+  });
+}
